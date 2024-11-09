d9a51f5b42f0e5ac5ef06a3b7fd52722ced21184
rhemesh <hemeshr07@gmail.com>
2024-11-09 11:58:05 +0530

    unique lock

--- unique_lock.cpp
+++ unique_lock.cpp
@@ -34,3 +34,11 @@ int main()
 //It works same as lock_guard but this one has some more features added in.unlike lock_guard in unique_lock we can lock() and unlock() whenever we want/
 //Whenever we create object for the unique lock it qutomatically lock the thread and if we don't want to do so then use defer and ypu can lock after sometime.
 //we can use conditional variable in unique lock();
+
+/*
+>It is an mutex ownership wrapper.It has a different locking stratagies they are:
+	.defer_lock()  ->Do not aquire ownership of the mutex
+	.try_to_lock() ->try to aquire ownership of the mutex without blocking
+	.adopt_lock()  ->assume that calling thread already has the ownership
+->It supports Time constrained attempts at locking (try_lock_for,try_lock_until)
+*/
