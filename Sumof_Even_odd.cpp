b30dd631c111f5196c81b789fef87f2bfa46f10e
rhemesh <hemeshr07@gmail.com>
2024-11-09 22:33:42 +0530

    Sum of even and odd numbers

--- Sumof_Even_odd.cpp
+++ Sumof_Even_odd.cpp
@@ -1,10 +1,12 @@
 #include<iostream>
 #include<thread>
-#include<mutex>
 #include<chrono>
+#include<algorithm>
 
 
 using namespace std;
+
+using namespace std::chrono;
 
 typedef long long int lli;
 
@@ -38,14 +40,24 @@ int main()
     printf("Enter start and end values to get sum of even and odd:\n");
     scanf("%lld %lld",&start,&end);
 
+    auto startTime = high_resolution_clock::now();
+
     thread t1(FindEvensum,start,end);
     thread t2(FindOddsum,start,end);
 
     t1.join();
     t2.join();
+    // FindEvensum(start,end);
+    // FindOddsum(start,end);
+
+    auto stopTime = high_resolution_clock::now();
+
+    auto duration = duration_cast<microseconds>(stopTime  - startTime);
 
     cout<<"Sum of Even numbers = "<<EvenSum<<endl;
     cout<<"Sum of Odd numbers = "<<OddSum<<endl;
+
+    cout<<"Seconds : "<<duration.count()/1000000<<endl;
 
     return 0;
 
